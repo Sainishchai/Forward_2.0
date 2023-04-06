db9b6bd19209905c22cda7ee4810ed295bd0e3df
Sainishchai <89191791+Sainishchai@users.noreply.github.com>
2023-04-06 14:20:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,16 +2,16 @@ import os
 import logging
 class Config:
     
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") 
+    API_ID = int(os.environ.get("API_ID", "28394784"))
+    API_HASH = os.environ.get("API_HASH", "9544a3ad7d8660acbae0dcf553c808e5")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6039043446:AAHf8ODju2FpNFBXj7v2-MwC0HxpLAu_0wU") 
     BOT_SESSION = os.environ.get("BOT_SESSION", "Forward_BOT") 
-    OWNER_ID = os.environ.get("OWNER_ID", "")
-    DATABASE_URI = os.environ.get("DATABASE_URI", "")
-    DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster0")
-    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Forward_data')
+    OWNER_ID = os.environ.get("OWNER_ID", "1172763005")
+    DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://forward:forward@cluster1.pgynkyr.mongodb.net/?retryWrites=true&w=majority")
+    DATABASE_NAME = os.environ.get("DATABASE_NAME","Cluster1")
+    COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'forward_files')
     SESSION = os.environ.get("SESSION", "Forward_Session")
-    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
+    TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001795790118"))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", None)
 
 
